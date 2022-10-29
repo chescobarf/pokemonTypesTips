a70ffafc600566d98cedd25f976aaff01dc7831f
Christian Escobar F <44533046+chescobarf@users.noreply.github.com>
2022-10-29 02:02:19 -0300

    Update TypeView

--- src/layouts/LayoutTypes.vue
+++ src/layouts/LayoutTypes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row gap-2 flex-wrap">
+  <div class="flex flex-row gap-2 flex-wrap mb-2">
     <slot></slot>
   </div>
 </template>
--- src/views/TypeView.vue
+++ src/views/TypeView.vue
@@ -1,47 +1,80 @@
 <template>
   <main class="container px-4 sm:mx-auto">
-    <div v-if="data">
-      <span class="text-lg">Doble Daño De:</span>
-      <LayoutTypes>
-        <template v-for="item in data.double_damage_from">
-          <Type :data="item" />
+    <h1 class="text-2xl capitalize mb-5">{{ route.params.name }} Type</h1>
+    <div v-if="data" class="grid md:grid-cols-2">
+      <div class="atacking">
+        <h2 class="text-2xl mb-3">Atacando</h2>
+        <template v-if="data.double_damage_to.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-green-700 text-white p-2 rounded-full">2x</span>
+            Doble Daño Hacia:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.double_damage_to">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
         </template>
-      </LayoutTypes>
+        <template v-if="data.half_damage_to.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-red-700 text-white p-2 rounded-full">½x</span>
+            Medio Daño Hacia:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.half_damage_to">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
+        </template>
 
-      <span class="text-lg">Doble Daño Hacia:</span>
-      <LayoutTypes>
-        <template v-for="item in data.double_damage_to">
-          <Type :data="item" />
+        <template v-if="data.no_damage_to.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-black text-white p-2 rounded-full">0x</span>
+            No le hace daño a:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.no_damage_to">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
         </template>
-      </LayoutTypes>
-
-      <span class="text-lg">Medio Daño De:</span>
-      <LayoutTypes>
-        <template v-for="item in data.half_damage_from">
-          <Type :data="item" />
+      </div>
+      <div class="defending">
+        <h2 class="text-2xl mb-3">Defendiendo</h2>
+        <template v-if="data.no_damage_from.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-black text-white p-2 rounded-full">0x</span>
+            Inmune a:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.no_damage_from">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
         </template>
-      </LayoutTypes>
-
-      <span class="text-lg">Medio Daño Hacia:</span>
-      <LayoutTypes>
-        <template v-for="item in data.half_damage_to">
-          <Type :data="item" />
+        <template v-if="data.half_damage_from.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-red-700 text-white p-2 rounded-full">½x</span>
+            Medio Daño De:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.half_damage_from">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
         </template>
-      </LayoutTypes>
-
-      <span class="text-lg">Inmune a:</span>
-      <LayoutTypes>
-        <template v-for="item in data.no_damage_from">
-          <Type :data="item" />
+        <template v-if="data.double_damage_from.length > 0">
+          <div class="text-lg mb-4">
+            <span class="bg-green-700 text-white p-2 rounded-full">2x</span>
+            Doble Daño De:
+          </div>
+          <LayoutTypes>
+            <template v-for="item in data.double_damage_from">
+              <Type :data="item" />
+            </template>
+          </LayoutTypes>
         </template>
-      </LayoutTypes>
-
-      <span class="text-lg">No le hace daño a:</span>
-      <LayoutTypes>
-        <template v-for="item in data.no_damage_to">
-          <Type :data="item" />
-        </template>
-      </LayoutTypes>
+      </div>
     </div>
   </main>
 </template>
