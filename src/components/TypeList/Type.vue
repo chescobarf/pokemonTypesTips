d8ddb805c9c9cbacb24c66111f31dc612e5e40d4
Christian Escobar F <44533046+chescobarf@users.noreply.github.com>
2022-10-29 00:57:13 -0300

    Fix routes and path icons

--- src/components/TypeList/Type.vue
+++ src/components/TypeList/Type.vue
@@ -22,7 +22,7 @@ const props = defineProps({
 const url = computed(() => "/type/" + props.data.name);
 
 const pathIcon = (type) => {
-  return `icons/${type}.svg`;
+  return `/icons/${type}.svg`;
 };
 </script>
 
